321eecc0169767a22f9212ca72cd864ffba9f8a0
Malnes <47932490+Malnes@users.noreply.github.com>
2021-02-18 10:51:54 +0100

    Add files via upload
    initial

---
+++ VikarApp-LA-setup-v1.3.ps1
@@ -0,0 +1,63 @@
+﻿<#
+This script creates a resource group and four logic apps
+
+PS: app registration must have the following permissions: 
+- group.readWrite.all
+- GroupMember.readWrite.all
+- user.read.all
+
+1. Install az module (install-module az)
+2. change directory (cd <path>) to logic app json template folder location
+3. edit "setup" variables
+4. run the script
+
+#>
+
+#setup
+$pathToTemplates         = ""    #spesicy the folder path where the logic apps templates is located e.g. C:\someFolder\VikarAppStuff
+$serviceUserPassword     = ""    # service user password
+$serviceUserUPN          = ""    #service user UPN
+$serviceUserNotification = ""    # service user to post teams messages
+$sharepointSiteUrl       = "https://<tenant>.sharepoint.com/sites/VikarApp321"    # Sharepoint site URL
+$tenantID                = ""    # Tenant ID
+$appID                   = ""    # App ID
+$appSecret               = ""    # App secret
+$subsctiptionId          = ""    # Subscription ID
+$classTeamIdentifier     = "Section_"    # Class team identifier. "use "Section_" for school data sync
+$logicAppPrefix          = ""    # Logic Apps Prefix (optional)"
+$rgPrefix                = ""    # Resource group prefix (optional)"
+$resourceLocation        = "West Europe"    # Resource location (e.g. West Europe)"
+$teacherGroupId          = ""    # teacher security group guid"
+$adminGroupId            = ""    # admin security group guid"
+
+
+#do not edit
+$appVerion = "v2"
+$nameAdd = $logicAppPrefix + "VikarApp-Add-" + $appVerion
+$nameArchive = $logicAppPrefix + "VikarApp-archive-" + $appVerion
+$nameQueue = $logicAppPrefix + "VikarApp-queue-" + $appVerion
+$nameAcl = $logicAppPrefix + "VikarApp-AdminAcl-" + $appVerion
+$resourceGroupName = $rgPrefix + "VikarApp"
+
+
+if($pathToTemplates.Substring($pathToTemplates.Length-1) -eq "/" -or $pathToTemplates.Substring($pathToTemplates.Length-1) -eq "\"){
+    $pathToTemplates = $pathToTemplates.Substring(0,$pathToTemplates.length-1)
+}
+
+
+if (Get-Module -ListAvailable -Name az.resources) {
+    write-host "Az module already installed"
+} 
+else {
+    Write-Host "Missing AZ module. Installing....."
+    install-module az
+}
+write-host "Sign in..."
+Connect-AzAccount
+Set-AzContext -SubscriptionId $subsctiptionId
+
+New-AzResourceGroup -name $resourceGroupName -location $resourceLocation
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -templateFile "$pathToTemplates\LA-WE-VikarApp-Add-v2.json" -logicAppName $nameAdd -paramadmin-securityGroup $adminGroupId -paramappID $appID -paramsecret $appSecret -paramserviceUser $serviceUserUPN -paramserviceUserNotifications $serviceUserNotification -paramsiteUrl $sharepointSiteUrl -paramteacher-securityGroup $teacherGroupId -paramteamIdentifier $classTeamIdentifier -paramtenantID $tenantID
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -templateFile "$pathToTemplates\LA-WE-VikarApp-archive-v2.json" -logicAppName $nameArchive -paramadmin-securityGroup $adminGroupId -paramappID $appID -paramsecret $appSecret -paramsiteURL $sharepointSiteUrl -paramteacher-securityGroup $teacherGroupId -paramteamIdentifier $classTeamIdentifier -paramtenantID $tenantID -azuread-1_displayName $serviceUserUPN
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -templateFile "$pathToTemplates\LA-WE-VikarApp-queue-v2.json" -logicAppName $nameQueue -paramappID $appID -paramclassTeamIdentifier $classTeamIdentifier -paramsecret $appSecret -paramsiteUrl $sharepointSiteUrl -paramtenantID $tenantID -paramusername $serviceUserUPN
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$pathToTemplates\LA-WE-VikarApp-AdminAcl-v2.json" -logicAppName $nameAcl -paramTeacherGroupId $teacherGroupId -paramadminGroupId $adminGroupId -paramsiteUrl $sharepointSiteUrl -azuread-1_token_TenantId $tenantID -azuread-1_displayName $serviceUserUPN
---
+++ VikarApp-SP-setup-v1.3.ps1
@@ -0,0 +1,100 @@
+﻿<#
+Created sharepoint site and lists used by the app.
+Remember to keep note of the site url as it will be used when provisioning the logic apps
+
+1. Install pnp (install-module pnp.PowerShell)
+2. Edit "setup" variables
+3. run script
+
+#>
+
+#Setup
+$sharepointRootUrl = "https://<tenant>.sharepoint.com"
+$siteName = "VikarApp"
+$siteUrl = "$sharepointRootUrl/sites/$siteName"
+$siteOwnerUpn = "<user upn>"
+
+#Connect and create site
+connect-pnponline -Url $siteUrl -Interactive
+
+$newSite = New-PnPSite -Type TeamSite -Title $siteName -Alias $siteName -Owners $siteOwnerUpn
+
+connect-pnponline -url $siteUrl -interactive
+
+
+#Add lists to the sharepoint site:
+
+# Create ACL list
+$listName = "ACLList"
+New-PnPList -Title $listName -Template GenericList
+Set-PnPField -List $listName -Identity Title -Values @{Required=$false}
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName name -InternalName name 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName userUpn -InternalName userUpn 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName admin -InternalName admin 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName teacher -InternalName teacher 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName groupAdmin -InternalName groupAdmin 
+
+# Create appArchive list
+$listName = "AppArchive"
+New-PnPList -Title $listName -Template GenericList
+Set-PnPField -List $listName -Identity Title -Values @{Required=$false}
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName substituteFor -InternalName substituteFor 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName substituteUpn -InternalName substituteUpn 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName teamName -InternalName teamName 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName school -InternalName school 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName teamId -InternalName teamId 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName status -InternalName status 
+Add-PnPField -list $listName -type DateTime -AddToDefaultView -DisplayName accessibleFrom -InternalName accessibleFrom 
+Add-PnPField -list $listName -type DateTime -AddToDefaultView -DisplayName accessibleTo -InternalName accessibleTo 
+
+# Create AppQueue list
+$listName = "AppQueue"
+New-PnPList -Title $listName -Template GenericList
+Set-PnPField -List $listName -Identity Title -Values @{Required=$false}
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName requester -InternalName requester 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName searchedUser -InternalName searchedUser 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName requestId -InternalName requestId 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName school -InternalName school 
+
+# Create AppQueueResponses list
+$listName = "AppQueueResponses"
+New-PnPList -Title $listName -Template GenericList
+Set-PnPField -List $listName -Identity Title -Values @{Required=$false}
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName requestId -InternalName requestId 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName searchedUser -InternalName searchedUser 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName requester -InternalName requester
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName teamId -InternalName teamId 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName teamName -InternalName teamName 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName school -InternalName school
+
+# Create AppRequests list
+$listName = "AppRequests"
+New-PnPList -Title $listName -Template GenericList
+Set-PnPField -List $listName -Identity Title -Values @{Required=$false}
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName substituteFor -InternalName substituteFor 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName substituteUpn -InternalName substituteUpn 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName teamName -InternalName teamName
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName teamId -InternalName teamId 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName status -InternalName status 
+Add-PnPField -list $listName -type DateTime -AddToDefaultView -DisplayName accessibleFrom -InternalName accessibleFrom 
+Add-PnPField -list $listName -type DateTime -AddToDefaultView -DisplayName accessibleTo -InternalName accessibleTo
+Add-PnPField -list $listName -type DateTime -AddToDefaultView -DisplayName extendTo -InternalName extendTo
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName school -InternalName school
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName isNew -InternalName isNew
+Add-PnPField -list $listName -type Number -AddToDefaultView -DisplayName timesExtended -InternalName timesExtended
+
+# Create AppSchoolAccess list
+$listName = "AppSchoolAccess"
+New-PnPList -Title $listName -Template GenericList
+Set-PnPField -List $listName -Identity Title -Values @{Required=$false}
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName schoolName -InternalName schoolName
+Add-PnPField -list $listName -type Note -AddToDefaultView -DisplayName hasAccessTo -InternalName hasAccessTo
+
+# Create AppSchoolAccess list
+$listName = "AppSchoolDisplayName"
+New-PnPList -Title $listName -Template GenericList
+Set-PnPField -List $listName -Identity Title -Values @{Required=$false}
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName schoolGroupName -InternalName schoolGroupName 
+Add-PnPField -list $listName -type Text -AddToDefaultView -DisplayName schoolDisplayName -InternalName schoolDisplayName 
+
+Disconnect-PnPOnline
